25038b7936fa6713d44d254268e953d787e9ea10
Ahmad Chaudhry <s224227027@deakin.edu.au>
2024-09-30 14:35:25 +1000

    test

--- start.sh
+++ start.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
-
-# Start the server in the background
 node controller.js &
-
-# Wait for the server to start (adjust as necessary)
 sleep 5
-
-# Run the tests
 npm test
-
-# Wait indefinitely to keep the container running
 wait
